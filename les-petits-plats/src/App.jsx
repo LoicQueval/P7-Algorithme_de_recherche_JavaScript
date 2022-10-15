9830728f9e71b67c272158d2b9195878d72c28fe
queva <queval.loic@gmail.com>
2022-10-16 01:37:31 +0200

    some clean, autocomplete ingredient/ustencil/appliance

--- les-petits-plats/src/App.jsx
+++ les-petits-plats/src/App.jsx
@@ -9,17 +9,67 @@ import {SearchTags} from './components/search-tags/search-tags';
 
 const App = () => {
     const [data, setData] = useState(undefined);
-const [query, setQuery] = useState('');
-    const [tags, updateTags] = useState('');
+    const [query, setQuery] = useState('');
+    const [tagsQuery, updateTagsQuery] = useState('');
+    const [appliancesQuery, updateAppliancesQuery] = useState('');
+    const [utensilsQuery, updateUtensilsQuery] = useState('');
+
+    const [allIngredients, setAllIngredients] = useState([]);
+    const [allUstensils, setAllUstensils] = useState([]);
+    const [allAppliances, setAllAppliances] = useState([]);
+
     useEffect(() => {
-        setData(search1(query, tags, recipes));
-    }, [query, tags]);
+        const ingredients = new Set();
+        const ustencils = new Set();
+        const appliances = new Set();
+
+        if (recipes === undefined) return;
+
+        recipes.forEach(recipe => {
+            const recipeIngredients = recipe['ingredients'];
+            const recipeUstencils = recipe['ustensils'];
+            const recipeAppliances = recipe['appliance'];
+
+            recipeIngredients.forEach((recipeIngredient) => {
+                    const ingredient = recipeIngredient['ingredient'][0].toUpperCase() + recipeIngredient['ingredient'].toLowerCase().slice(1)
+                    ingredient.includes(tagsQuery) ? ingredients.add(ingredient) : console.log('no ingredient');
+                }
+            );
+
+            recipeUstencils.forEach((recipeUstencil) => {
+                    const ustencil = recipeUstencil[0].toUpperCase() + recipeUstencil.toLowerCase().slice(1)
+                    ustencil.includes(utensilsQuery) ? ustencils.add(ustencil) : console.log('no ustencil');
+                }
+            );
+
+            const appliance = recipeAppliances[0].toUpperCase() + recipeAppliances.toLowerCase().slice(1)
+            appliance.includes(appliancesQuery) ? appliances.add(appliance) : console.log('no appliances');
+        });
+
+        setAllIngredients(Array.from(ingredients));
+        setAllUstensils(Array.from(ustencils));
+        setAllAppliances(Array.from(appliances));
+
+    }, [tagsQuery, utensilsQuery, appliancesQuery]);
+
+    useEffect(() => {
+        setData(search1(query, tagsQuery, utensilsQuery, appliancesQuery, recipes));
+        /*setData(search2(query, tagsQuery, utensilsQuery, appliancesQuery, recipes));*/
+    }, [query, tagsQuery, utensilsQuery, appliancesQuery]);
 
     return (
         <>
             <Header/>
             <SearchBar updateSearchQuery={setQuery}/>
-            <SearchTags updateTagsQuery={updateTags}/>
+            <SearchTags tags={allIngredients} updateTagsQuery={updateTagsQuery}
+                        placeholderName="Cherchez des ingredients"
+                        color="blue"/>
+            <SearchTags tags={allUstensils} updateTagsQuery={updateUtensilsQuery}
+                        placeholderName="Cherchez des appareils"
+                        color="red"/>
+            <SearchTags tags={allAppliances} updateTagsQuery={updateAppliancesQuery}
+                        placeholderName="Cherchez des ustensiles"
+                        color="green"/>
             <Receipts data={data}/>
         </>
     );
--- les-petits-plats/src/components/search-tags/search-tags.jsx
+++ les-petits-plats/src/components/search-tags/search-tags.jsx
@@ -1,13 +1,26 @@
 import './search-tags.css';
+import {useState} from 'react';
 
-export const SearchTags = ({updateTagsQuery}) => {
-    const onTags = (e) =>{
+export const SearchTags = ({tags, updateTagsQuery, placeholderName, color}) => {
+    const [filteredTags, updateFilteredTags] = useState(tags);
+
+    const onTagSelected = (e) => {
+        // updateTagsQuery(e.target.value);
+    }
+
+    const onTagQueryChange = (e) => {
         updateTagsQuery(e.target.value);
     }
 
+
     return (
-        <div>
-            <input type="text" placeholder="Ingredient" onChange={onTags}/>
+        <div id="toto" className={color}>
+            <input type="text" placeholder={placeholderName} onChange={onTagQueryChange} className={color}/>
+            <div>
+                {tags ? tags.map((tag, index) => (
+                    <p onClick={onTagSelected} key={`${index}`}>{tag}</p>
+                )) : null}
+            </div>
         </div>
     )
 }
